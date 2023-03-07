0892818ba7cd4d1ecfe73c58d7417efdb72e84bf
卫慧杰 <w920098695@sina.cn>
2023-03-07 21:22:47 +0800

    style: 登录页面部分布局

--- src/views/login/login-page.vue
+++ src/views/login/login-page.vue
@@ -1,3 +1,26 @@
 <template>
-	<div>login</div>
+	<div class="login-page">
+		<div class="login-box">
+			<!--  -->
+		</div>
+	</div>
 </template>
+
+<style lang="less" scoped>
+.login {
+	&-page {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+		background-color: var(--background-color);
+	}
+	&-box {
+		box-shadow: 0px 10px 20px 0px #dbe7ee;
+		width: 1128px;
+		height: 700px;
+		background: #ffffff;
+		border-radius: 8px;
+	}
+}
+</style>
